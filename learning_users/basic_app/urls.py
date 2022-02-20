a16e3f1fcbad6dbdb64bb19b8d10c93b3d10272e
yadavajay9527 <yadavaj9527@gmail.com>
2022-02-20 08:22:26 +0530

    first

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from basic_app import views
+
+app_name = 'basic_app'
+urlpatterns = [
+    path('register/', views.register,name='register'),
+    path('login/', views.user_login,name='user_login'),
+]
